2000a5042ac08e257c8d3456597f9e4dbbe77694
alejandro871 <alejandro.barahona@correounivalle,edu,co>
2026-04-02 02:40:41 -0500

    Se crea jugador con Arraylist e hice un pequeño testeo en el main

--- Main.java
+++ Main.java
@@ -1,13 +1,23 @@
 import src.cartas.CartaMagica;
+import src.cartas.Mounstruo;
+import src.jugadores.Jugador;
 
 
 public class Main {
 
     public static void main(String[] arg) {
 
+        Jugador jugador = new Jugador("Alejo");
+
+        Mounstruo dragon = new Mounstruo("Dragoncillo", 3000, 2500, 10);
+
         CartaMagica magia = new CartaMagica("mago electrico");
 
-        magia.activar();
+
+        jugador.agregarCarta (dragon);
+        jugador.agregarCarta(magia);
+
+        jugador.mostrarCartas();
 
     }
     
--- src/jugadores/Jugador.java
+++ src/jugadores/Jugador.java
@@ -1,5 +1,51 @@
 package src.jugadores;
+import java.util.ArrayList;
+import src.cartas.Carta;
+
 
 public class Jugador {
     
+    private String nombre;
+    private int vida;
+    private ArrayList<Carta> cartas; //es una lista de cartas que tiene el jugaodr
+    //el <carta> es porq solo se pueden en esa lista los objetos cartas
+    public Jugador (String nombre){
+
+        this.nombre = nombre;
+        this.vida = 8000; //inica directamente con la vida estipulada ya que no varia
+        this.cartas = new ArrayList<>(); //nuevo bolsillo pa las cartas
+
+    }
+
+    public void agregarCarta(Carta carta){
+
+        cartas.add(carta);
+
+    }
+
+    public void mostrarCartas(){
+
+        for (Carta c: cartas) {
+
+            System.out.println(c.grtNombre());
+
+        }
+
+    }
+
+    public int getVida(){
+
+        return vida;
+
+    }
+
+public String getNombre(){
+
+    return nombre;
+
+}
+
+
+
+
 }
